755d777271b23555c3cc802bddcda62e40b26429
Camila Melo <contato.camilamelo@outlook.com>
2021-01-02 14:20:09 -0300

    modificações no Date e usando o map

--- js/app/controllers/NegociacaoController.js
+++ js/app/controllers/NegociacaoController.js
@@ -14,8 +14,17 @@ class NegociacaoController {
 
           event.preventDefault();
         
-          let data = new Date(this._inputData.value.split('-'))
-          console.log(data)
-  
+          let data = new Date(
+              ...this._inputData.value
+                .split('-')
+                .map((item, indice) => item - indice % 2)
+            );
+
+            let negociacao = new Negociacao(
+                data,
+                this._inputQuantidade.value,
+                this._inputValor.value
+            );
+            console.log(negociacao);
     }
   }
